5e2258d2f5170c41f9d33747d417e8ae0545dff2
devjldp <joseluisdp297@gmail.com>
2024-02-05 13:09:49 +0000

    Add register functionality to app.py

--- app.py
+++ app.py
@@ -28,10 +28,49 @@ mongo = PyMongo(app)
 
 # Routes
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+# Register a new user 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Check if the username already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # Check if the email already exists in the database
+        existing_email = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+    
+        if existing_user or existing_email:
+            # If the user already exists, display an error message and redirect to the registration page
+            flash("Username /email already exists")
+            return redirect(url_for("register"))
+    
+        # Register the new user in the database
+        register = {
+            "name": request.form.get("name").lower(),
+            "email": request.form.get("email").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        
+        # insert the new user in my db
+        mongo.db.users.insert_one(register)
+        
+        # Start a session for the new user
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful!")
+    
+    return render_template("register.html")
+
+
+
+
 @app.route('/books')
 def getBooks():
-  books = mongo.db.books.find()
-  return render_template("books.html", books=books)
+    books = mongo.db.books.find()
+    return render_template("books.html", books=books)
 
 
 
